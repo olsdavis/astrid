7ef888dd0becde3b226da4a8f259b7115291daa8
Oscar Davis <olsdavis@gmail.com>
2020-02-18 15:35:43 +0100

    Fixed Polynomial#toString, added first unit tests.

--- src/ch/epfl/rigel/math/Polynomial.java
+++ src/ch/epfl/rigel/math/Polynomial.java
@@ -4,6 +4,9 @@ import static ch.epfl.rigel.Preconditions.checkArgument;
 
 /**
  * Class used for the representation of a polynomial with positive integer exponents.
+ *
+ * @author Alexandre Doukhan
+ * @author Oscar Davis
  */
 public class Polynomial {
     private final double[] coefficients;
@@ -17,15 +20,15 @@ public class Polynomial {
      *
      * @param coeffDom     Corresponds to the dominant coefficient of the polynom.
      *                     Will be placed at the start of the coefficients list.
-     * @param coefficients The rest of the coefficients. Will be placed after coeffDom
+     * @param c The rest of the coefficients. Will be placed after coeffDom
      * @return a new Polynomial instance with given coefficients.
      */
-    public Polynomial of(double coeffDom, double... coefficients) {
-        checkArgument(coeffDom == 0);
-        double[] coeffs_list = new double[coefficients.length + 1];
-        coeffs_list[0] = coeffDom;
-        System.arraycopy(coefficients, 0, coeffs_list, 1, coeffs_list.length);
-        return new Polynomial(coeffs_list);
+    public static Polynomial of(double coeffDom, double... c) {
+        checkArgument(coeffDom != 0);
+        double[] coefficients = new double[c.length + 1];
+        coefficients[0] = coeffDom;
+        System.arraycopy(c, 0, coefficients, 1, c.length);
+        return new Polynomial(coefficients);
     }
 
     /**
@@ -51,14 +54,32 @@ public class Polynomial {
     public String toString() {
         StringBuilder builder = new StringBuilder();
         for (int i = 0; i < coefficients.length; i++) {
-            if (!(coefficients[i] == 0)) {
-                builder.append(coefficients[i]);
-                builder.append("x^");
-                builder.append(coefficients.length - 1 - i);
-                builder.append(' ');
-            }
-            if (i < coefficients.length - 1 && coefficients[i + 1] > 0) {
-                builder.append('+');
+            // do not write 0x^c
+            if (coefficients[i] != 0) {
+                double coefficient = coefficients[i];
+                // do not write -1x^c nor 1x^c
+                if (Math.abs(coefficient) == 1.0d) {
+                    // add the minus if the coefficient is -1
+                    if (coefficient == -1.0d) {
+                        builder.append("-");
+                    }
+                } else {
+                    builder.append(coefficient);
+                }
+                int power = coefficients.length - 1 - i;
+                // write x^c if c is not null (constant coefficient)
+                if (power != 0) {
+                    builder.append("x");
+                    // do not write x^1
+                    if (power != 1) {
+                        builder.append('^').append(power);
+                    }
+                }
+
+                // if it is not the last element, and the next element is positive
+                if (i < coefficients.length - 1 && coefficients[i + 1] > 0) {
+                    builder.append('+');
+                }
             }
         }
         return builder.toString();
