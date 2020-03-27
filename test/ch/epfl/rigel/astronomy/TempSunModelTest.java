e2aaf075ae630418abf66ffdb765f46eeaf15e77
Oscar Davis <olsdavis@gmail.com>
2020-03-27 01:10:39 +0100

    Duplicate test + fix expected vs actual.

--- test/ch/epfl/rigel/astronomy/TempSunModelTest.java
+++ test/ch/epfl/rigel/astronomy/TempSunModelTest.java
@@ -22,17 +22,15 @@ public class TempSunModelTest {
                         LocalTime.of(0, 0), ZoneOffset.UTC))).equatorialPos().ra());
         assertEquals(19.35288373097352, SunModel.SUN.at(-2349, new EclipticToEquatorialConversion(ZonedDateTime.of(LocalDate.of(2003, Month.JULY, 27),
                 LocalTime.of(0, 0, 0, 0), ZoneOffset.UTC))).equatorialPos().decDeg());
-        assertEquals(SunModel.SUN.at(-2349, new EclipticToEquatorialConversion(ZonedDateTime.of(LocalDate.of(2003, Month.JULY,
-                27), LocalTime.of(0, 0, 0, 0), ZoneOffset.UTC))).equatorialPos().raHr(), 8.3926828082978, 1e-14);
-        assertEquals(19.35288373097352, SunModel.SUN.at(-2349, new EclipticToEquatorialConversion(ZonedDateTime.of(LocalDate.of(2003, Month.JULY,
-                27), LocalTime.of(0, 0, 0, 0), ZoneOffset.UTC))).equatorialPos().decDeg());
+        assertEquals(8.3926828082978, SunModel.SUN.at(-2349, new EclipticToEquatorialConversion(ZonedDateTime.of(LocalDate.of(2003, Month.JULY, 27),
+                LocalTime.of(0, 0, 0, 0), ZoneOffset.UTC))).equatorialPos().raHr(), 1e-14);
         ZonedDateTime zdt = ZonedDateTime.of(LocalDate.of(1988, Month.JULY, 27), LocalTime.of(0, 0), ZoneOffset.UTC);
-        assertEquals(SunModel.SUN.at(J2010.daysUntil(zdt),
-                new EclipticToEquatorialConversion(zdt)).equatorialPos().dec(), 0.3353207024580374);
+        assertEquals(0.3353207024580374, SunModel.SUN.at(J2010.daysUntil(zdt),
+                new EclipticToEquatorialConversion(zdt)).equatorialPos().dec());
         ZonedDateTime zone1988 = ZonedDateTime.of(
                 LocalDate.of(1988, Month.JULY, 27),
                 LocalTime.of(0, 0), ZoneOffset.UTC);
-        assertEquals(SunModel.SUN.at(J2010.daysUntil(zone1988), new EclipticToEquatorialConversion(zone1988)).angularSize(), 0.009162353351712227, 1e-10);
+        assertEquals(SunModel.SUN.at(J2010.daysUntil(zone1988), new EclipticToEquatorialConversion(zone1988)).angularSize(), 0.009162353351712227, 1e-14);
     }
 
 }
