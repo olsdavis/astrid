6290935adffc4308883caff7f957e9b28b92486c
Oscar Davis <olsdavis@gmail.com>
2020-04-27 10:18:57 +0200

    Doc + format.

--- src/ch/epfl/rigel/gui/NamedTimeAccelerator.java
+++ src/ch/epfl/rigel/gui/NamedTimeAccelerator.java
@@ -13,32 +13,54 @@ import java.util.concurrent.TimeUnit;
  */
 public enum NamedTimeAccelerator {
 
+    /**
+     * Real time continuous accelerator.
+     */
     TIMES_1(
             "1×",
             TimeAccelerator.continuous(1L)
     ),
+    /**
+     * 30 times faster continuous accelerator.
+     */
     TIMES_30(
             "30×",
             TimeAccelerator.continuous(30L)
     ),
+    /**
+     * 300 times faster continuous accelerator.
+     */
     TIMES_300(
             "300×",
             TimeAccelerator.continuous(300L)
     ),
+    /**
+     * 3000 times faster continuous accelerator.
+     */
     TIMES_3000(
             "3000×",
             TimeAccelerator.continuous(3000L)
     ),
+    /**
+     * Day per day discrete accelerator.
+     */
     DAY(
             "jour",
-            TimeAccelerator.discrete(TimeUnit.SECONDS.toNanos(60),
-                    Duration.ofNanos(TimeUnit.DAYS.toNanos(1)))
+            TimeAccelerator.discrete(
+                    TimeUnit.SECONDS.toNanos(60),
+                    Duration.ofNanos(TimeUnit.DAYS.toNanos(1))
+            )
     ),
+    /**
+     * Sidereal day per sidereal day discrete accelerator.
+     */
     SIDEREAL_DAY(
             "jour sidéral",
-            TimeAccelerator.discrete(TimeUnit.SECONDS.toNanos(60),
+            TimeAccelerator.discrete(
+                    TimeUnit.SECONDS.toNanos(60),
                     // 23h56m04s in seconds
-                    Duration.ofNanos(TimeUnit.SECONDS.toNanos(23 * 3600 + 56 * 60 + 4)))
+                    Duration.ofNanos(TimeUnit.SECONDS.toNanos(23 * 3600 + 56 * 60 + 4))
+            )
     );
 
     private final String name;
