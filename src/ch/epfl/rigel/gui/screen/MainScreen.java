61d44c8b36c70977b98563c793e88daccf10a972
olsdavis <olsdavis@gmail.com>
2020-05-27 17:04:57 +0200

    WIP. Small changes.

--- src/ch/epfl/rigel/gui/screen/MainScreen.java
+++ src/ch/epfl/rigel/gui/screen/MainScreen.java
@@ -1,10 +1,8 @@
 package ch.epfl.rigel.gui.screen;
 
-import ch.epfl.rigel.util.Fonts;
 import javafx.animation.AnimationTimer;
 import javafx.animation.FadeTransition;
 import javafx.animation.Timeline;
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.CacheHint;
 import javafx.scene.Node;
@@ -14,7 +12,6 @@ import javafx.scene.control.Button;
 import javafx.scene.input.MouseButton;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.util.Duration;
 
@@ -46,8 +43,6 @@ public final class MainScreen implements Screen {
         }
     }
 
-    private static final Font MAIN_FONT = Fonts.loadUnsafe("/PatrickHandSC-Regular.ttf");
-
     private final StackPane root = new StackPane();
 
     /**
@@ -61,13 +56,11 @@ public final class MainScreen implements Screen {
 
         // top pane: title
         final Text text = new Text("RIGEL");
-        text.setFont(MAIN_FONT);
-        text.setFill(Color.WHITESMOKE);
         text.getStyleClass().add("main-title");
         // little blink animation
         final FadeTransition transition = new FadeTransition(Duration.seconds(2d), text);
         transition.setFromValue(1.0d);
-        transition.setToValue(0.1);
+        transition.setToValue(0.1d);
         transition.setCycleCount(Timeline.INDEFINITE);
         transition.setAutoReverse(true);
         transition.play();
@@ -84,11 +77,8 @@ public final class MainScreen implements Screen {
                 event.consume();
             }
         });
-        // TODO: change font of buttons!!
         final VBox buttons = new VBox(start, about);
-        buttons.setSpacing(25d);
-        buttons.setAlignment(Pos.CENTER);
-        buttons.setPadding(new Insets(-150d, 0, 0, 0));
+        buttons.getStyleClass().add("main-buttons-group");
         final GridPane center = grid(Pos.CENTER, buttons);
         pane.setCenter(center);
         pane.setBackground(Background.EMPTY);
@@ -107,7 +97,6 @@ public final class MainScreen implements Screen {
      */
     private Button button(String text) {
         final Button ret = new Button(text);
-        ret.setFont(MAIN_FONT);
         ret.getStyleClass().add("main-button");
         return ret;
     }
